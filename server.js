632831045237543e3c595c2904fe2d4a5a0d2e76
24HP6473 24HP6473 <24HP6473@admins-iMac.local>
2024-12-03 18:18:04 +0800

    cors

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const {mongoose} = require("mongoose");
+const { mongoose } = require("mongoose");
 const express = require("express");
 const userModel = require("./Module/userSchema");
-const postRoute = require("./Route/postRoute")
+const postRoute = require("./Route/postRoute");
 const app = express();
 const userRoute = require("./Route/userRoute");
-const commentRoute = require("./Route/commentRoute")
-const likeRoute = require("./Route/likeRoute")
+const commentRoute = require("./Route/commentRoute");
+const likeRoute = require("./Route/likeRoute");
+const cors = require("cors");
 
 const connectDataBase = async () => {
-  console.log("call connect db")
+  console.log("call connect db");
   try {
     const res = await mongoose.connect(
       "mongodb+srv://inguuninguun7:Pinecone123@cluster0.iy551.mongodb.net/instagram?retryWrites=true&w=majority&appName=Cluster0"
@@ -20,26 +21,21 @@ const connectDataBase = async () => {
 };
 
 connectDataBase();
-
-
+app.use(cors());
 app.use(express.json());
-app.use(userRoute)
-app.use(postRoute)
-app.use(likeRoute)
-app.use(commentRoute)
+app.use(userRoute);
+app.use(postRoute);
+app.use(likeRoute);
+app.use(commentRoute);
 
+app.get("/", async (req, res) => {
+  res.send("working");
+});
 
-
-
-
-app.get('/', async (req, res) => {
-  res.send("working")
-})
-
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   try {
-    const user = req.body;  
-    await userModel.create(user); 
+    const user = req.body;
+    await userModel.create(user);
     res.send("User created successfully");
   } catch (error) {
     console.error(error);
@@ -48,5 +44,5 @@ app.post('/users', async (req, res) => {
 });
 
 app.listen(8000, () => {
-  console.log('http://localhost:8000');
+  console.log("http://localhost:8000");
 });
