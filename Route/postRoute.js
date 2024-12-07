8e5c7534bac086592af57d304778a03457217021
24HP6473 24HP6473 <24HP6473@admins-iMac.local>
2024-12-07 10:07:06 +0800

    first commit

--- Route/postRoute.js
+++ Route/postRoute.js
@@ -1,58 +1,62 @@
 const Route = require("express");
-const postModel  = require("../Module/postSchema");
+const postModel = require("../Module/postSchema");
 const userModel = require("../Module/userSchema");
 const commentModel = require("../Module/commentSchema");
 
 const postRoute = Route();
 
-postRoute.post('/writeComment', async(req, res) => {
-    try {
-            const { comment, postId, userId } = req.body;
-    
+postRoute.post("/writeComment", async (req, res) => {
+  try {
+    const { comment, postId, userId } = req.body;
+
     const respond = await commentModel.create({
-        comment,
-        postId,
-        userId
+      comment,
+      postId,
+      userId,
     });
-    res.status(200).json(respond)
-    } catch (error) {
-        console.log(error)
-        res.send(error)
-    }
-    })
+    res.status(200).json(respond);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 
 postRoute.post("/post", async (req, res) => {
-    const { caption, postImage, userId } = req.body
-    try{
+  const { caption, postImage, userId } = req.body;
+  try {
     const createdPost = await postModel.create({
-        caption,
-        postImage,
-        userId,
+      caption,
+      postImage,
+      userId,
     });
     await userModel.findByIdAndUpdate(userId, {
-        $push: {
-            posts: createdPost._id,
-        }
-    })
+      $push: {
+        posts: createdPost._id,
+      },
+    });
     res.status(200).json(createdPost);
-   } catch (error) {
-    res.status(500).json(error)
-   }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-postRoute.get("/post", async(req, res) =>{
-  const posts =  await postModel.find().populate('userId', "email username _id")
-  res.status(200).json(posts)
-})
+postRoute.get("/post", async (req, res) => {
+  const posts = await postModel
+    .find()
+    .populate("userId", "email username profileImage _id");
+  res.status(200).json(posts);
+});
 
-
-postRoute.get("/posts", async(req, res) =>{
-    const posts = await postModel.find().populate("userId", "email username _id").populate({
-    path: 'likes',
-    populate: {
-        path: 'userId',
-        select: "username profileImage"
-
-}})
-})
+postRoute.get("/posts", async (req, res) => {
+  const posts = await postModel
+    .find()
+    .populate("userId", "email username _id")
+    .populate({
+      path: "likes",
+      populate: {
+        path: "userId",
+        select: "username profileImage",
+      },
+    });
+});
 module.exports = postRoute;
